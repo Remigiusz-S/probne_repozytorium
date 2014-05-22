b74572648e8ff98389e44f2abc237cd1218351a8
Remigiusz-S <remig.s.krak@gmail.com>
2014-05-22 14:39:25 +0200

    Opis zmian

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo 'to index<br />';
+
+include 'jakis_plik.php';
+
+
+
+
+
+
+
+
+
+
